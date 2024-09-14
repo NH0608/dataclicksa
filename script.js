ca2ba1712fc674f17959fab14c8f1a3778b421a8
NH0608 <nrosero65@misena.edu.co>
2024-09-13 19:40:20 -0500

    Actualización de la funcionalidad X

---
+++ script.js
@@ -0,0 +1,3 @@
+function openMap() {
+    window.open("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127450.10950218266!2d-76.52566395000001!3d3.3950644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a6f0cc4bb3f1%3A0x1f0fb5e952ae6168!2sCali%2C%20Valle%20del%20Cauca!5e0!3m2!1ses-419!2sco!4v1726260678762!5m2!1ses-419!2sco", "popup", "width=600,height=600");
+  }
